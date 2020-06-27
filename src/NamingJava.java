ce99ac2661581629d3218278c6bb25a4585f67a8
Pirris <luissalas_10@outlook.com>
2020-06-26 19:40:24 -0600

    Se crea la clase namingJava para demostrar como declarar variables en Java

---
+++ src/NamingJava.java
@@ -0,0 +1,22 @@
+public class NamingJava {
+
+    public static void main(String[] args){
+
+        int cellphone = 556542;
+        int cellPhone = 8462147;
+        System.out.println(cellphone);
+        System.out.println(cellPhone);
+
+        String $coutryName = "Costa Rica";
+        String _backgroundColor = "Black";
+        String currency$ = "CRC";
+        String background_Color ="BLUE";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+
+    }
+
+}
