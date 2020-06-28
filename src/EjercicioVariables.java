123546e225460aa53d821b552d788d6697db8ab1
Pirris <luissalas_10@outlook.com>
2020-06-27 22:32:22 -0600

    Se concluye el ejercicio 1 de la clase #21 de Java SE Platzi

---
+++ src/EjercicioVariables.java
@@ -0,0 +1,23 @@
+public class EjercicioVariables {
+
+    public static void main(String[] args){
+
+        String padre = "Fernando";
+        String madre = "Brenda";
+        String hijo = "Luis";
+        String hijaMayor = "Isennya";
+        String hijaMenor = "Zuilyn";
+        String apellidoPadre = "Salas";
+        String apellidoMadre = "Salgado";
+
+        System.out.println("Bienvenidos a la familia: " + apellidoPadre + " " + apellidoMadre + ".");
+        System.out.println("Su composición es la siguiente:");
+        System.out.println("Padre de familia: " + padre + " " + apellidoPadre + ".");
+        System.out.println("Madre de familia: " + madre + " " + apellidoMadre + ".");
+        System.out.println("Hijo paciente: " + hijo + " " + apellidoPadre + " " + apellidoMadre + ".");
+        System.out.println("Hija loca: " + hijaMayor + " " + apellidoPadre + " " + apellidoMadre + ".");
+        System.out.println("Hija rebelde: " + hijaMenor + " " + apellidoPadre + " " + apellidoMadre + ".");
+
+    }
+
+}
