c4a4ca0038f16f26fa40a9644be56a8b471e585b
Pirris <luissalas_10@outlook.com>
2020-06-27 18:04:49 -0600

    Se crea una clase para practicar incrementos y decrementos

---
+++ src/IncrementDecrement.java
@@ -0,0 +1,26 @@
+public class IncrementDecrement {
+
+    public static void main(String[] args){
+
+        int lives = 5;
+        lives = lives -1;
+        System.out.println(lives);
+
+        lives--; //Decremento
+        System.out.println(lives);
+
+        lives++; // Incremento
+        System.out.println(lives);
+
+        //Prefija
+        // Gana un regalo por ganar una vida
+
+        int gift = 100 + lives ++;
+        System.out.println(gift);
+
+        ++lives;
+        System.out.println(lives);
+
+    }
+
+}
