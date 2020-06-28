dee46da2c995be57b6a64337448078877558fecc
Pirris <luissalas_10@outlook.com>
2020-06-27 19:39:37 -0600

    Se crean ejercicios para aprender acerca del casteo entre tipos de datos, char a int, char a short, int a double

--- src/Casting.java
+++ src/Casting.java
@@ -17,6 +17,24 @@ public class Casting {
         int b = 12;
         System.out.println((double) a/b);
 
+
+        // CASTEO ENTRE TIPOS DE DATOS
+
+        //Convirtiendo de int a double, a y b son int, por lo que el resultado de c será entero a no ser que lo
+        //convirtamos a double.
+        double c = (double) a/b;
+        System.out.println(c);
+
+        //convirtiendo de char a int, el resultado es 49 por que en código ACSII 1 equivale a 49. Char como variable es
+        //menor que int, por lo que la conversión ser realiza de manera automática
+        char n = '1';
+        int nI = n;
+        System.out.println(nI);
+
+        //convirtiendo de char s short, char es un dato más grande que short, por lo que si requiere convertirlo
+        // mediante casting
+        short nS = (short) n;
+        System.out.println(nS);
     }
 
 }
